1099247b9fc5de174b7f19ab83fc0c70df071513
d1mas2k3 <dvkab@mail.ru>
2025-05-14 21:01:39 +0300

    Anther change

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(errorHandler);
 const start = async() => {
     try {
         await sequelize.authenticate();
-        // await sequelize.sync();
+        await sequelize.sync();
         app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
     } catch (e) {
         console.log(e);
